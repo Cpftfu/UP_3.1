900f29b78a4eb759c69970a1956f444c7dc0c1a6
Cpftfu <alexseev45s@gmail.com>
2024-04-14 23:41:40 +0300

    третий практос на EntityFramework

---
+++ pageForFullGrid.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UP_3._1
+{
+	/// <summary>
+	/// Логика взаимодействия для pageForFullGrid.xaml
+	/// </summary>
+	public partial class pageForFullGrid : Page
+	{
+		private MagazineEntities context = new MagazineEntities();
+
+		public pageForFullGrid()
+		{
+			InitializeComponent();
+			fullDataGrid.ItemsSource = context.Clients.ToList();
+		}
+	}
+}
